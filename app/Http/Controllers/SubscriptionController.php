4ae32f3d88e5e5944747e9e346d03022b699cbed
guiandradedev <guiandradeemail@gmail.com>
2024-01-05 00:05:18 -0300

    feat: show permissions & roles, create subscription

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,28 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\SubscriptionStatus;
 use App\Enums\SystemEnum;
+use App\Events\SubscriptionCreatedEvent;
+use App\Models\Subscription;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class SubscriptionController extends Controller
 {
+    function __construct(
+        protected Permission $permission,
+        protected Role $role,
+        protected Subscription $subscription
+    )
+    {
+        
+    }
+    // Permissions
     public function permissions(Request $request) {
-        $permissions = Permission::where('system', SystemEnum::COMMERCIAL->name)->with('roles')->paginate($request->get('per_page', 5), ['*'], 'page', $request->get('page', 1));
+        $permissions = $this->permission->where('system', SystemEnum::COMMERCIAL->name)->with('roles')->paginate($request->get('per_page', 5), ['*'], 'page', $request->get('page', 1));
         // dd($permissions[0]->roles[0]);
         return view('subscription.permissions.index', ['permissions'=>$permissions]);
     }
+    public function add_role() {
 
+    }
+    public function show_permission(Request $request) {
+        $permission = $this->permission->where('system', SystemEnum::COMMERCIAL->name)->where('name', $request->permission)->with('roles')->get()->first();
+        if(!$permission) {
+            return redirect()->route('buffet.subscription')->withErrors('slug', "Permission not found.");
+        }
+
+        // dd($permission->permissions);
+        return view('subscription.permissions.show', ['permission'=>$permission]);
+    }
+    // Roles
     public function roles(Request $request) {
-        $roles = Role::where('system', SystemEnum::COMMERCIAL->name)->with('permissions')->paginate($request->get('per_page', 5), ['*'], 'page', $request->get('page', 1));
+        $roles = $this->role->where('system', SystemEnum::COMMERCIAL->name)->with('permissions')->paginate($request->get('per_page', 5), ['*'], 'page', $request->get('page', 1));
         return view('subscription.roles.index', ['roles'=>$roles]);
     }
 
     public function show_role(Request $request) {
-        $role = Role::where('system', SystemEnum::COMMERCIAL->name)->where('name', $request->role)->with('permissions')->get();
-        // dd($permissions);
-        return view('subscription.roles.show', ['roles'=>$role]);
+        $role = $this->role->where('system', SystemEnum::COMMERCIAL->name)->where('name', $request->role)->with('permissions')->get()->first();
+        if(!$role) {
+            return redirect()->route('buffet.subscription')->withErrors('slug', "Role not found.");
+        }
+
+        // dd($role->permissions);
+        return view('subscription.roles.show', ['role'=>$role]);
     }
 
     public function create_role() {
@@ -33,30 +61,49 @@ class SubscriptionController extends Controller
     public function store_role() {
 
     }
-
-    public function add_role() {
-
-    }
-    public function show_permission() {
-        return view('subscription.permissions.show');
-    }
-
+    // Subscriptions
     public function create_subscription() {
         return view('subscription.create');
     }
 
-    public function store_subscription() {
+    public function store_subscription(Request $request) {
+        $subscription_exists = $this->subscription->where('slug', $request->slug)->get()->first();
+        if($subscription_exists) {
+            return back()->withErrors('slug', "Subscription already exists.")->withInput();
+        }
 
+        $subscription = $this->subscription->create([
+            'name'=>$request->name,
+            'slug'=>$request->slug,
+            'description'=>$request->description,
+            'price'=>$request->price,
+            'status'=>SubscriptionStatus::ACTIVE->name
+        ]);
+
+        event(new SubscriptionCreatedEvent($subscription));
+
+        return redirect()->route('buffet.subscription.show', ['subscription'=>$subscription->slug]);
     }
     
-    public function subscriptions() {
-        return view('subscription.index');
-
+    public function subscriptions(Request $request) {
+        $subscriptions = $this->subscription->paginate($request->get('per_page', 5), ['*'], 'page', $request->get('page', 1));
+        return view('subscription.index', ['subscriptions'=>$subscriptions]);
     }
 
-    public function show_subscription() {
-        return view('subscription.roles.show');
+    public function show_subscription(Request $request) {
+        $subscription = $this->subscription->where('slug', $request->subscription)->get()->first();
+        if(!$subscription) {
+            return redirect()->route('subscription.index')->withErrors('not_found', 'Subscription not found');
+        }
+        return view('subscription.show', ['subscription'=>$subscription]);
     }
+    public function edit_subscription() {
+        dd('edit');
+    }
+    public function update_subscription() {
+        dd('update');
+    }
+
     // API
 
     public function get_roles_by_permission_api(Request $request) {
--- database/migrations/2024_01_04_205357_create_subscriptions_table.php
+++ database/migrations/2024_01_04_205357_create_subscriptions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\SubscriptionStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,7 +18,8 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->text('description');
             $table->float('price');
-            $table->float('discount');
+            $table->float('discount')->default(0);
+            $table->enum('status', array_column(SubscriptionStatus::cases(), 'name'));
             $table->timestamps();
         });
     }
